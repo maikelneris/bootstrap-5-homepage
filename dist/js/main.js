a74e1b20511d97e60ddff967cace7a7b2724e185
maikelneris <maikel@decodeweb.com.br>
2020-07-03 20:17:01 -0300

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function ($) {
+  window.onscroll = function () {
+    if (window.pageYOffset > 140) {
+      $("#header").addClass("active");
+    } else {
+      $("#header").removeClass("active");
+    }
+  };
+});
