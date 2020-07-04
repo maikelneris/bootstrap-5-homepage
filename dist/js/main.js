80c1a7d31e8ceb606acd0afe8fdd6e5d8a223b42
maikelneris <maikel@decodeweb.com.br>
2020-07-04 11:43:14 -0300

    Finalização do layout

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function ($) {
+  //FIXED HEADER
   window.onscroll = function () {
     if (window.pageYOffset > 140) {
       $("#header").addClass("active");
@@ -6,4 +7,52 @@ jQuery(document).ready(function ($) {
       $("#header").removeClass("active");
     }
   };
+
+  //OWL
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    autoplay: false,
+    autoplayTimeout: 6000,
+    dots: true,
+    lazyLoad: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+
+  //ISOTOPE
+  let btns = $("#servicos .button-group button");
+
+  btns.click(function (e) {
+    $("#servicos .button-group button").removeClass("active");
+    e.target.classList.add("active");
+
+    let selector = $(e.target).attr("data-filter");
+    $("#servicos .grid").isotope({
+      filter: selector,
+    });
+  });
+
+  $("#btn-all").trigger("click");
+
+  //MAGNIFY
+  $(".grid .popup-link").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+      tPrev: "Anterior",
+      tNext: "Próxima",
+      tCounter: "%curr% de %total%",
+    },
+  });
 });
